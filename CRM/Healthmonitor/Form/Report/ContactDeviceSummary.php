5eec42af6b3aa79dc70a63e4478a9ccc924eb2a4
octohindol <hindol@octopus8.com>
2021-09-03 15:09:57 +0300

    filters for summary done

--- CRM/Healthmonitor/Form/Report/ContactDeviceSummary.php
+++ CRM/Healthmonitor/Form/Report/ContactDeviceSummary.php
@@ -149,11 +149,12 @@ class CRM_Healthmonitor_Form_Report_ContactDeviceSummary extends CRM_Report_Form
                         ],
                     ],
                     'filters' => [
-                        'sensor_id' => [
+                        'data_sensor_id' => [
+                            'name' => 'sensor_id',
                             'title' => ts('Data Sensor'),
                             'type' => CRM_Utils_Type::T_STRING,
                             'operatorType' => CRM_Report_Form::OP_MULTISELECT,
-                            'options' => CRM_Core_PseudoConstant::get("CRM_Healthmonitor_BAO_HealthAlarmRule", "sensor_id"),
+                            'options' => CRM_Core_PseudoConstant::get("CRM_Healthmonitor_BAO_HealthMonitor", "sensor_id"),
                         ],
 //                        $sensor_name = CRM_Core_PseudoConstant::getLabel("CRM_Healthmonitor_BAO_HealthAlarmRule", "sensor_id");
                     ],
@@ -171,7 +172,8 @@ class CRM_Healthmonitor_Form_Report_ContactDeviceSummary extends CRM_Report_Form
                         ],
                     ],
                     'filters' => [
-                        'sensor_id' => [
+                        'rule_sensor_id' => [
+                            'name' => 'sensor_id',
                             'title' => ts('Alarm Rule Sensor'),
                             'type' => CRM_Utils_Type::T_STRING,
                             'operatorType' => CRM_Report_Form::OP_MULTISELECT,
